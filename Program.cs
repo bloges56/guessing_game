92e1b53d29b3009bd284e5e0a1ec7ccee6919968
bloges56 <brady.drew.logan@gmail.com>
2020-11-16 10:32:44 -0600

    number of guesses now determined by a user chosen difficulty

--- Program.cs
+++ Program.cs
@@ -6,6 +6,31 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            //Prompt for a difficulty level
+            Console.WriteLine("Choose a difficulty");
+            Console.WriteLine("1: Easy");
+            Console.WriteLine("2: Medium");
+            Console.WriteLine("3: Hard");
+            Console.Write("Your Choice: ");
+
+            //Save their choice
+            int difficulty = Int32.Parse(Console.ReadLine());
+
+            //determine the number of guesses based off the difficulty chosen
+            int guesses;
+            if(difficulty == 1)
+            {
+                guesses = 8;
+            }
+            else if(difficulty == 2)
+            {
+                guesses = 6;
+            }
+            else
+            {
+                guesses = 4;
+            }
+
             //Prompt for a guess
             Console.WriteLine("Guess the secret number!");
 
@@ -13,8 +38,8 @@ namespace GuessingGame
             Random r = new Random();
             int secretNum = r.Next(1,101);
 
-            //Give the user four chances
-            for (int i = 4; i >=1; i--)
+            //Give the user the number of chances determined by the difficulty
+            for (int i = guesses; i >=1; i--)
             {
                 //Prompt
                 if(i == 1){
