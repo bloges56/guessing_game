ef96fc0bd154010d0077364042d9e78ad5b09622
bloges56 <brady.drew.logan@gmail.com>
2020-11-16 10:18:13 -0600

    the secret number is now random and the number of guesses left is displayed

--- Program.cs
+++ Program.cs
@@ -9,29 +9,44 @@ namespace GuessingGame
             //Prompt for a guess
             Console.WriteLine("Guess the secret number!");
 
-            //declare and init secretNum
-            int secreteNum = 42;
+            //create a random integer for the user to guess
+            Random r = new Random();
+            int secretNum = r.Next(1,101);
 
             //Give the user four chances
-            for (int i = 1; i <= 4; i++)
+            for (int i = 4; i >=1; i--)
             {
-                Console.Write($"Guess {i}: ");
+                //Prompt
+                if(i == 1){
+                    Console.Write($"{i} Guess left: ");
+                }
+                else
+                {
+                    Console.Write($"{i} Guesses left: ");
+                }
+                
                 //save the guess
                 int guess = Int32.Parse(Console.ReadLine());
 
                 //compare the guess and secretNum
-                if (guess == secreteNum)
+                if (guess == secretNum)
                 {
                     //if they are equal display success message
                     Console.WriteLine("You Win!");
                     //break out of loop
                     break;
                 }
-                else
+                else if(i == 1)
                 {
-                    //else display failure message
+                    //else if its the last turn display the lose message
                     Console.WriteLine("You Lose!");
                 }
+                else
+                {
+                    //otherwise display the failure message
+                    Console.WriteLine("Wrong!");
+                }
+
             }
 
 
